e3674c7c6114e360a01c75128c8ce5fe9404bfe0
Crowdbotics <engineering@crowdbotics.com>
2022-08-19 17:28:56 +0000

    Update models

--- backend/hfdd_dev_29689/wsgi.py
+++ backend/hfdd_dev_29689/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hfdd_dev_29689.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hfdd_dev_29689.settings")
 
 application = get_wsgi_application()
